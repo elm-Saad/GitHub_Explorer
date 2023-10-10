fd8b8648fb92c2e9dbfbfd11ec19dd6b79e1cc92
Saad <128216930+elm-Saad@users.noreply.github.com>
2023-10-10 08:11:34 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import { Box } from '@mui/material';
 
 import Home from './page/Home';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 
 export default function App() {
   return (
   <Box width="400px" sx={{ width: { xl: '1490px' } }} m="auto" className='bg-gray-100 bg-opacity-50 min-h-screen'>
     <Navbar />
     <Home />
-    <Footer />
   </Box>
   )
 }
