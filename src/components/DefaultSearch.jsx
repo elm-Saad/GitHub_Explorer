5b0d86a1de5f4c06aee2845f6081752a216119b6
Saad <128216930+elm-Saad@users.noreply.github.com>
2023-10-10 09:30:34 +0200

    update

--- src/components/DefaultSearch.jsx
+++ src/components/DefaultSearch.jsx
@@ -1,9 +1,26 @@
 import React from "react"
-
+import Container from '@mui/material/Container'
+import GitHub from "../assets/git.webp"
 export default function Defaultsearch(){
     return (
         <>
-            <p>no data</p>
+          <Container
+            maxWidth="sm"
+            sx={{
+                textAlign: 'center',
+                py: 4,
+                paddingTop:0,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+            >   
+                <img
+                    className="rounded-full overflow-hidden w-96 h-64 " 
+                    src={GitHub} alt="GitHub image"
+                />
+            </Container>
         </>
     )
 }
